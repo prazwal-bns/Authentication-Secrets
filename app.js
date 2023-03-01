a0d50ab26e39f76dc0b14751336192df83777f7c
Prajwal Banstola <prajwalbns15@gmail.com>
2023-03-01 18:06:30 +0545

    2nd level of Security Hashing

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption')
+var md5 = require('md5');
 
 const app = express();
-
 app.use(express.static("public"));
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
@@ -20,8 +19,6 @@ const userSchema = new mongoose.Schema ( {
     password: String
 });
 
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields: ['password']});
 
 const User =  mongoose.model("User",userSchema);
 
@@ -41,7 +38,7 @@ app.get("/register", function(req,res){
 app.post("/register",function(req,res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save(function(err){
@@ -56,7 +53,7 @@ app.post("/register",function(req,res){
 
 app.post("/login",function(req,res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username},function(err,foundUser){
         if(err){
